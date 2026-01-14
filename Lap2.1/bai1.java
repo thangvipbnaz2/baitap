6adddca7fe41471b5f7f0f5915fd11b54561c0ad
thangvipbnaz2 <tn8018074@gmail.com>
2026-01-14 09:31:48 +0700

    Delete Lap2.1 directory

--- Lap2.1/bai1.java
+++
@@ -1,6 +0,0 @@
-public class bai1 {
-    public static void main(String[] args) {
-        System.out.println("Xin chào Java!");
-        System.out.println("Tôi đang học lập trình Java.");
-    }
-}
